6020975235a4acee28b16daa7157de65b21ecb5a
clintonace <clintonace09@gmail.com>
2024-10-22 08:55:24 +0100

    header/body/footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,57 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<head>
+  <meta charset="utf-8">
+  <title>I&M Limited</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+  <link href="/onboarding/css/bootstrap.css" rel="stylesheet">
+  <link href="/onboarding/css/style.css" rel="stylesheet">
+  <link href="/onboarding/css/responsive.css" rel="stylesheet">
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+
+<body data-anm=".anm">
+
+  <div class="page-wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+    <x-headerlayout/>
+
+    <x-bodylayout/>
+
+    <x-footerlayout/>
+
+
+  </div><!-- End Page Wrapper -->
+
+
+  <script src="/onboarding/js/jquery.js"></script>
+  <script src="/onboarding/js/popper.min.js"></script>
+  <script src="/onboarding/js/chosen.min.js"></script>
+  <script src="/onboarding/js/bootstrap.min.js"></script>
+  <script src="/onboarding/js/jquery.fancybox.js"></script>
+  <script src="/onboarding/js/jquery.modal.min.js"></script>
+  <script src="/onboarding/js/mmenu.polyfills.js"></script>
+  <script src="/onboarding/js/mmenu.js"></script>
+  <script src="/onboarding/js/appear.js"></script>
+  <script src="/onboarding/js/anm.min.js"></script>
+  <script src="/onboarding/js/ScrollMagic.min.js"></script>
+  <script src="/onboarding/js/rellax.min.js"></script>
+  <script src="/onboarding/js/owl.js"></script>
+  <script src="/onboarding/js/wow.js"></script>
+  <script src="/onboarding/js/script.js"></script>
+
+</body>
+
+
 </html>
